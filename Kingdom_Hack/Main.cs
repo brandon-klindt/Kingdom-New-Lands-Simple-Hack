50dfd5615986ceac04eb53e46cd098e53f3ebe69
Brandon Klindt <brandon.klindt98@gmail.com>
2020-11-01 22:04:38 +0200

    Add project files.

---
+++ Kingdom_Hack/Main.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Kingdom_Hack
+{
+    class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+            _player = FindObjectOfType<Player>();
+            _damageable = FindObjectOfType<Damageable>();
+        }
+
+        public void Update()
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                Loader.Unload();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                _damageable.invulnerable = !_damageable.invulnerable;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                _player.coins += 10;
+            }
+        }
+
+        public void OnGUI()
+        {
+            GUI.Label(new Rect(10, 10, 150f, 50f), "v1.0.0 by spliff\nPlayer Coins: " + _player.coins + "\nInvulnerable : " + _damageable.invulnerable);
+        }
+        private Player _player;
+        private Damageable _damageable;
+
+    }
+}
